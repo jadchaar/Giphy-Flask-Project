66bea447d0cb0528a7a6420622548728476dcd99
jadchaar~ <jadchaar@umich.edu>
2017-02-27 22:09:23 -0500

    error handling procedures (e.g. blank string)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, Markup
 from giphy_translation import translate_gif
 
 app = Flask(__name__)
@@ -8,13 +8,17 @@ def index():
     if request.method == 'POST':
         text = request.form['inputText']
         # Changes spaces to dashes
+        text = text.strip()
         if text.find(" ") != -1:
             text = text.replace(" ", "-")
         # Checks if blank string is inputted
         if not text:
-            # display error to re input string
-        giphyURL = translate_gif(text)
-        return render_template("form.html", giphy=giphyURL)
+            error = "Please input a valid gif name \U0001F642"
+            giphyURL = "https://media1.giphy.com/media/13NRvWtOiMXawM/200.gif"
+            return render_template("form.html", giphy=giphyURL, error = error)
+        else:
+            giphyURL = translate_gif(text)
+            return render_template("form.html", giphy=giphyURL)
     else:
         return render_template("form.html")
 
