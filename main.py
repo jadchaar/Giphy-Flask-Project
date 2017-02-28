571a2daa7ae7d56a8c6178f16595e46d7b95c259
jadchaar~ <jadchaar@umich.edu>
2017-02-28 16:48:19 -0500

    Added button to randomly generate gif

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request
 from giphy_translation import translate_gif
 from giphy_random import random_gif
 
@@ -8,22 +8,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        text = request.form['inputText']
-
-        # Changes spaces to dashes
-        text = text.strip()
-        if text.find(" ") != -1:
-            text = text.replace(" ", "-")
-
-        # Checks if blank string is inputted
-        if not text:
-            error = "Please input a valid gif name \U0001F642"
-            giphyURL = "https://media1.giphy.com/media/13NRvWtOiMXawM/200.gif"
-            source = "https://giphy.com/gifs/things-13NRvWtOiMXawM"
-            return render_template("form.html", giphy=giphyURL, error=error, source=source)
-        else:
-            giphyURL = translate_gif(text)
+        if "random" in request.form:
+            giphyURL = random_gif()
             return render_template("form.html", giphy=giphyURL[0], source=giphyURL[1])
+        else:
+            text = request.form['inputText']
+
+            # Changes spaces to dashes
+            text = text.strip()
+            if text.find(" ") != -1:
+                text = text.replace(" ", "-")
+
+            # Checks if blank string is inputted
+            if not text:
+                error = "Please input a valid gif name \U0001F642"
+                giphyURL = "https://media1.giphy.com/media/13NRvWtOiMXawM/200.gif"
+                source = "https://giphy.com/gifs/things-13NRvWtOiMXawM"
+                return render_template("form.html", giphy=giphyURL, error=error, source=source)
+            else:
+                giphyURL = translate_gif(text)
+                return render_template("form.html", giphy=giphyURL[0], source=giphyURL[1])
     else:
         # Default welcome gif is loaded
         giphyURL = "https://media2.giphy.com/media/l0MYC0LajbaPoEADu/200.gif"
